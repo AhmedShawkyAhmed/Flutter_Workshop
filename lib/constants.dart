7505fefc91dee62d17e1cf073a30463fa3eb3dd9
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-01 21:46:49 +0200

    Update

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,10 @@ import 'package:flutter/material.dart';
 
 Color white = Colors.white;
 Color blue = Colors.blue;
+Color lightBlue = const Color(0xFFCEE3FF);
 Color black = Colors.black;
 Color grey = Colors.grey;
+Color green = Colors.green;
+Color lightGreen = const Color(0xFFDDFFDD);
+Color red = Colors.red;
+Color lightRed = const Color(0xFFFFE1E1);
