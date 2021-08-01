549d98b4b787e38b0365d1044a08acbee334e96f
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-01 20:28:17 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workshop/screens/post.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const LinkedinPost(),
     );
   }
 }
