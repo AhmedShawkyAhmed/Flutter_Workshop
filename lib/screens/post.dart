72451670a5994a6f10221a045afa013bd109bbc2
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-09-06 12:46:40 +0200

    Modified

--- lib/screens/post.dart
+++ lib/screens/post.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_workshop/constants.dart';
+import 'package:flutter_workshop/colors.dart';
 
 class LinkedinPost extends StatelessWidget {
   const LinkedinPost({Key? key}) : super(key: key);
